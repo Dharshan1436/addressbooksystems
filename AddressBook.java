2ff7cf221161627ba8606e57de091eb1990c678c
Dharshan <dharshan1436@gmail.com>
2024-09-05 13:06:11 +0530

    creating address book system and creating contacts

---
+++ AddressBook.java
@@ -0,0 +1,13 @@
+package addressbooksystems;
+
+
+
+
+public class AddressBook {
+	// Welcome message
+    public static void welcomeMsg() {
+        System.out.println("Welcome to the Address Book System!");
+      
+        
+    }
+}
---
+++ AddressBookTest.java
@@ -0,0 +1,10 @@
+package addressbooksystems;
+
+public class AddressBookTest {
+
+	public static void main(String[] args) {
+	AddressBook.welcomeMsg();
+
+	}
+
+}
